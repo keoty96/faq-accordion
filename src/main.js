0e762d845dc2da14a027a7ba4caed10301dbb84d
Katherine Osorio <fanfour007@gmail.com>
2021-06-08 12:21:46 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+function showAnswers(question, arrow, answer) {
+    console.log(question);
+
+    switch (question) {
+        case "q1":
+            changeStyle(question, arrow, answer);
+            break;
+        case "q2":
+            changeStyle(question, arrow, answer);
+            break;
+        case "q3":
+            changeStyle(question, arrow, answer);
+            break;
+        case "q4":
+            changeStyle(question, arrow, answer);
+            break;
+        case "q5":
+            changeStyle(question, arrow, answer);
+            break;
+
+        default:
+            break;
+    }
+}
+
+function changeStyle(question, arrow, answer) {
+
+    let element = document.getElementById(question);
+
+    if (element.style.fontWeight !== "700") {
+        document.getElementById(question).style.fontWeight = "700";
+        document.getElementById(arrow).className = "rotateimg180";
+        document.getElementById(answer).style.display = "block"
+    } else {
+        document.getElementById(question).style.fontWeight = "400";
+        document.getElementById(arrow).classList.remove("rotateimg180");
+        document.getElementById(answer).style.display = "none"
+    }
+}
